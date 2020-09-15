6320c128bebd84fe4c7b5817cc7c953274d51daa
kangsanguk <63863758+kangsanguk@users.noreply.github.com>
2020-09-16 00:08:25 +0900

    Add Python practice

---
+++ variable2.py
@@ -0,0 +1,10 @@
+data = 10
+name = "강상욱"
+age = 10
+height = 180.00
+#print는 두번 사용한다.
+#print("이름 : ", end = '')
+#print(name)
+print("이름 : %s입니다." %name)
+print("나이 : %d살" %age)
+print("키 : %.2f" %height)
