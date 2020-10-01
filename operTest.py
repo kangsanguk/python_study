2a2b74a35203ba966da6e92be0e751ec25627a64
kangsanguk <63863758+kangsanguk@users.noreply.github.com>
2020-10-01 23:44:59 +0900

    Add Python practice

---
+++ operTest.py
@@ -0,0 +1,18 @@
+print(10>4)
+print(10 != 1 and 10 > 4)
+print(10 == 1 or 10 > 4)
+
+isTrue = 20>3
+print(isTrue)
+
+name = input("이름 : ")
+isTrue = name == "강상욱"
+print(isTrue)
+print(not isTrue)
+
+condition1 = 10 > 5
+condition2 = 10 == 1
+
+condition3 = condition1  and condition2
+
+print(condition3)
